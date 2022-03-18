e8e9f09b711cc614569c5fa55f0b13d49cfe9f70
thmsrbrt <30445476+thmsrbrt@users.noreply.github.com>
2022-03-18 17:25:26 +0100

    update component questionnaire

--- src/components/Questionnaire.vue
+++ src/components/Questionnaire.vue
@@ -1,12 +1,12 @@
 <template>
-  <div class="container mt-5">
-    <form class="w-50" @submit.prevent="submitForm">
+  <div class="container">
+    <form @submit.prevent="submitForm">
       <div class="mb-3">
         <p>Quel lait a été utilisé pour fabriquer {{ fromage }} ?</p>
       </div>
       <div class="mb-3">
         <label for="optionsLait" class="form-label">Type de lait</label>
-        <select id="optionsLait" v-model="selected">
+        <select id="optionsLait">
           <option value="Vache" selected>Vache</option>
           <option value="chevre">Chèvre</option>
           <option value="brebie">Brebie</option>
@@ -15,22 +15,36 @@
       <button type="submit" class="btn btn-primary">Valider</button>
     </form>
   </div>
+  <div v-if="result" class="mt-lg-4">
+    <div class="alert alert-success">
+      Bonne réponse !
+    </div>
+  </div>
+  <div v-else class="mt-lg-4">
+    <div class="alert alert-warning">
+      Perdu, vous pouvez rejouer!
+    </div>
+  </div>
 </template>
 
 <script>
 
 export default {
-  name: "Questionnaire",
+  name: `Questionnaire`,
   data() {
     return {
       typeLait: ["Vache", "Chèvre", "Brebie"],
       selected: '',
-      fromage: ''
+      fromage: '',
+      result: '',
+      styleResult:''
     }
   },
   methods: {
     submitForm() {
-      //TODO vérifier le choix dans la bdd ou script dans leaflet
+      //TODO vérifie r le choix dans la bdd ou script dans leaflet
+      var res = true
+      this.result = res;
     }
   }
 }
--- src/components/SideBar.vue
+++ src/components/SideBar.vue
@@ -1,14 +1,36 @@
 <template>
   <div class="col-sm-2">
-    <div>
-      <h1>Chercher le jeu </h1>
+    <div v-if="etat">
+      <h1>Le jeu </h1>
+      <questionnaire></questionnaire>
+    </div>
+    <div v-else>
+      <h3>Veuillez sélectionner une région pour accéder au jeu</h3>
     </div>
   </div>
 </template>
 
 <script>
+import Questionnaire from "@/components/Questionnaire";
 export default {
-  name: "SideBar"
+  name: "SideBar",
+  components: {
+    // eslint-disable-next-line vue/no-unused-components
+    Questionnaire
+  },
+  data() {
+    return {
+      etat: 'true' // TODO metter '' pour le départ
+    }
+  },
+  methods() {
+    var fromage = true; //TODO : envoyer une info quand l'user un sélection une région
+    if (fromage) {
+      this.etat = true;
+    } else {
+      this.etat = false;
+    }
+  }
 }
 </script>
 
